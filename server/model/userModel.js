effe5d59549a4fa8fe2d2426e6e020d53c8b24a2
ShravanDeshmukh2305 <149171559+ShravanDeshmukh2305@users.noreply.github.com>
2024-09-06 16:33:03 +0530

    -- add full Stack App

---
+++ server/model/userModel.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+    fname:{
+        type: String,
+        required: true
+    },
+    lname:{
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+
+    password:{
+        type: String,
+        required: true
+    },
+
+    email:{
+        type: String,
+        required: true
+    },
+    priority: {
+        type: String,
+        enum: ['Low', 'Medium', 'High'],
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+
+    }
+
+})
+
+
+export default mongoose.model("User", userSchema);
